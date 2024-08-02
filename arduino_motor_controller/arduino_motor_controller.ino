18bbdd5d4fd994e08d9a305eb9d79a88a17b994c
SVBSH <getenv@protonmail.com>
2024-08-02 22:57:22 +0200

    add suppoprt for 4 motors

--- arduino_motor_controller/arduino_motor_controller.ino
+++ arduino_motor_controller/arduino_motor_controller.ino
@@ -1,32 +1,68 @@
-const int ledPin = 13; // Pripojte LED na pin 13 (na doskách s integrovanou LED)
+#include <AFMotor.h>
 
-void setup() {
+const int LED_PIN = 13;
+const int MOTOR_FREQUENCY = 255;
+
+AF_DCMotor motor_1(1);
+AF_DCMotor motor_2(2);
+AF_DCMotor motor_3(3);
+AF_DCMotor motor_4(4);
+
+void setup()
+{
   // Inicializácia sériovej komunikácie
   Serial.begin(9600);
-  // Inicializácia LED pinu ako výstup
-  pinMode(ledPin, OUTPUT);
-  // Uistite sa, že LED je vypnutá pri štarte
-  digitalWrite(ledPin, HIGH); // Zapnúť LED
-  delay(3000);
-  digitalWrite(ledPin, LOW);
-  
+  initLED();
+  motor_1.setSpeed(MOTOR_FREQUENCY);
+  motor_2.setSpeed(MOTOR_FREQUENCY);
+  motor_3.setSpeed(MOTOR_FREQUENCY);
+  motor_4.setSpeed(MOTOR_FREQUENCY);
 }
 
-void loop() {
+void initLED()
+{
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, HIGH);
+  delay(3000);
+  digitalWrite(LED_PIN, LOW);
+}
+
+void loop()
+{
+  motor_1.run(BACKWARD);
+  motor_2.run(BACKWARD);
+  motor_3.run(BACKWARD);
+  motor_4.run(BACKWARD);
+  delay(3000);
+
+  motor_1.run(RELEASE);
+  motor_2.run(RELEASE);
+  motor_3.run(RELEASE);
+  motor_4.run(RELEASE);
+
+  delay(2000);
+
   // Skontrolujte, či sú k dispozícii dáta zo sériovej linky
-  if (Serial.available() > 0) {
-    String command = Serial.readStringUntil('\n');
+  // if (Serial.available() > 0)
+  // {
+  //   String command = Serial.readStringUntil('\n');
+  //   Serial.print("Command:");
+  //   Serial.println(command);
+  //   command.trim();
+  //   Serial.println("");
 
-    Serial.print("Command:");
-    Serial.println(command);
-    command.trim();
-    Serial.println("");
+  //   handleCommand(command);
+  // }
+}
 
-
-    if (command == "LEDON") {
-      digitalWrite(ledPin, HIGH); // Zapnúť LED
-    } else if (command == "LEDOFF") {
-      digitalWrite(ledPin, LOW); // Vypnúť LED
-    }
+void handleCommand(String command)
+{
+  if (command == "LEDON")
+  {
+    digitalWrite(LED_PIN, HIGH); // Zapnúť LED
+  }
+  else if (command == "LEDOFF")
+  {
+    digitalWrite(LED_PIN, LOW); // Vypnúť LED
   }
 }
